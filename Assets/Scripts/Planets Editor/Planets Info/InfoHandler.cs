c3ce85dbf2fe51a0f0e5ed63ad468d4f3d361139
Marcel Marusiak <marcel.marusiak@gmail.com>
2023-01-13 19:59:35 +0100

    WIP: Planet editor & creator - planet's info window Added planet appearance and name loader to info window.

--- Assets/Scripts/Planets Editor/Planets Info/InfoHandler.cs
+++ Assets/Scripts/Planets Editor/Planets Info/InfoHandler.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InfoHandler : MonoBehaviour
 {
     public GameObject AttachedPlanet;
+    public Image MainPlanetImg;
+    public Image BackgroundPlanetImg;
 
     void Update()
     {
@@ -12,6 +15,14 @@ public class InfoHandler : MonoBehaviour
     public void LoadInfoData(GameObject dataContainer)
     {
         var gravityForce = dataContainer.GetComponent<GravityObject>().CurrentGravityForceVector;
+        transform.Find("MenuPanel").Find("PlanetNameTXT").GetComponent<Text>().text = dataContainer.name + "'s info";
+
+        SpriteRenderer planetSprite = dataContainer.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        MainPlanetImg.color = planetSprite.color;
+        MainPlanetImg.sprite = planetSprite.sprite;
+        BackgroundPlanetImg.color = new Color(planetSprite.color.r, planetSprite.color.g, planetSprite.color.b, 0.35f);
+        BackgroundPlanetImg.sprite = planetSprite.sprite;
+        
         Debug.Log(gravityForce);
     }
 
