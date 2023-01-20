7bc3fae21456db4dec3e15eea8f8712d1a892d83
Marcel Marusiak <marcel.marusiak@gmail.com>
2023-01-20 21:25:25 +0100

    Polished: Vectors editor - displaying values properly on first opening

--- Assets/Scripts/Planets Editor/Velocity Editor/VelocityComponents.cs
+++ Assets/Scripts/Planets Editor/Velocity Editor/VelocityComponents.cs
@@ -10,7 +10,7 @@ public class VelocityComponents : MonoBehaviour
     private InputField xField, yField;
     [SerializeField] private Text magnitudeText;
 
-    private bool updatedOnChange = true;
+    private bool updatedOnChange = false;
     private bool updatedOnReset = false;
     
     // Start is called before the first frame update
@@ -30,62 +30,58 @@ public class VelocityComponents : MonoBehaviour
             // unpaused
             if (Time.timeScale > 0)
             {
+                updatedOnChange = false;
                 updatedOnReset = false;
-
-                // update Texts
-                xField.text = VectorsController.Velocity.x.ToString(CultureInfo.InvariantCulture);
-                yField.text = VectorsController.Velocity.y.ToString(CultureInfo.InvariantCulture);
-                magnitudeText.text = VectorsController.Velocity.magnitude.ToString(CultureInfo.InvariantCulture);
+                
+                UpdateTexts(VectorsController.Velocity);
             }
 
             // paused
             else if (GravityObjectsController.Instance.Reseted && !updatedOnReset)
-            { 
-                updatedOnReset = true; // dont look at this loop :)
+            {
+                updatedOnChange = false;
+                updatedOnReset = true; 
+                
+                // dont look at this loop :)
                 while (!VectorsController.Ready) { }
-                xField.text = GlobalVariables.Instance.CurrentGravityObject.InitialVelocity.x.ToString(CultureInfo
-                    .InvariantCulture);
-                yField.text = GlobalVariables.Instance.CurrentGravityObject.InitialVelocity.y.ToString(CultureInfo
-                    .InvariantCulture);
-                magnitudeText.text = GlobalVariables.Instance.CurrentGravityObject.InitialVelocity.magnitude.ToString(CultureInfo
-                    .InvariantCulture);
+                
+                UpdateTexts(GlobalVariables.Instance.CurrentGravityObject.InitialVelocity);
             }
-            
+            else if (Time.timeScale == 0 && !updatedOnChange)
+            {
+                updatedOnChange = true;
+                
+                UpdateTexts(VectorsController.Velocity);
+            }
+
         }
     }
 
-    public void SubmitValue(int type)
+    void UpdateTexts(Vector3 vel)
+    {
+        xField.text = vel.x.ToString(CultureInfo.InvariantCulture);
+        yField.text = vel.y.ToString(CultureInfo.InvariantCulture);
+        magnitudeText.text = vel.magnitude.ToString(CultureInfo.InvariantCulture);
+    }
+
+    public void SubmitValue()
     {
         if (Time.timeScale == 0)
         {
             
             float valueX = float.Parse(xField.text, CultureInfo.InvariantCulture.NumberFormat);
-
-            if(!GravityObjectsController.Instance.Reseted)
-            { 
-                GlobalVariables.Instance.CurrentGravityObject.GetComponent<Rigidbody2D>().velocity =
-                    new Vector2(valueX, GlobalVariables.Instance.CurrentGravityObject.GetComponent<Rigidbody2D>().velocity.y);
-            }
-
-            GlobalVariables.Instance.CurrentGravityObject.InitialVelocity.x = valueX;
-            
             float valueY = float.Parse(yField.text, CultureInfo.InvariantCulture.NumberFormat);
 
-            if(!GravityObjectsController.Instance.Reseted) 
-            { 
-                GlobalVariables.Instance.CurrentGravityObject.GetComponent<Rigidbody2D>().velocity = 
-                    new Vector2(GlobalVariables.Instance.CurrentGravityObject.GetComponent<Rigidbody2D>().velocity.x, valueY);
-                        
+            if (GravityObjectsController.Instance.Reseted)
+            {
+                GlobalVariables.Instance.CurrentGravityObject.InitialVelocity.x = valueX;
+                GlobalVariables.Instance.CurrentGravityObject.InitialVelocity.y = valueY;
             }
-                    
-            GlobalVariables.Instance.CurrentGravityObject.InitialVelocity.y = valueY;
             
+            GlobalVariables.Instance.CurrentGravityObject.GetComponent<Rigidbody2D>().velocity =
+                new Vector2(valueX, valueY);
+
         }
-        
-        if(GravityObjectsController.Instance.Reseted) 
-            GlobalVariables.Instance.CurrentGravityObject.UpdatePlanet(); 
-        
-        updatedOnChange = false;
     } 
 }
 
