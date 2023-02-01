9a7148534d50d8cc89120c255c26255ddaeab132
Marcel Marusiak <marcel.marusiak@gmail.com>
2023-02-01 17:31:15 +0100

    Bug fix: Reseting selected planet after new planet created

--- Assets/Scripts/GlobalVariables.cs
+++ Assets/Scripts/GlobalVariables.cs
@@ -24,8 +24,5 @@ public class GlobalVariables : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(GameObject.FindWithTag("EditorController").GetComponent<EditorHandler>().Planet != null)
-            CurrentGravityObject = GameObject.FindWithTag("EditorController").GetComponent<EditorHandler>().Planet
-                .GetComponent<GravityObject>();
     }
 }
