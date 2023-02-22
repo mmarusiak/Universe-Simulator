b89af12b0344b086d2657e0d48c027ecaeaad294
Marcel Marusiak <marcel.marusiak@gmail.com>
2023-02-22 15:10:33 +0100

    Small fixes: Added panel for detecting clicks on save container

--- Assets/Scripts/Menu/SavesScrollbar/SaveContainer.cs
+++ Assets/Scripts/Menu/SavesScrollbar/SaveContainer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,7 +9,7 @@ public class SaveContainer : MonoBehaviour
     private string _saveName, _pathToSave;
     private Sprite _saveCapture;
     private Image _savePreviewContainer;
-    private Text _saveText;
+    private Text _saveText, _dateText;
     private RectTransform _view;
     private DateTime _lastModified;
 
@@ -18,19 +19,23 @@ public class SaveContainer : MonoBehaviour
     public async Task Initialize(string saveName)
     {
         _view = transform.GetChild(0).GetComponent<RectTransform>();
-        _savePreviewContainer = _view.transform.Find("SavePreview").GetComponent<Image>();
-        _saveText = _view.transform.Find("SaveName").GetComponent<Text>();
+        _savePreviewContainer = _view.GetChild(0).transform.Find("SavePreview").GetComponent<Image>();
+        _saveText = _view.GetChild(0).transform.Find("SaveName").GetComponent<Text>();
+        _dateText = _view.GetChild(0).transform.Find("DateText").GetComponent<Text>();
         
         _pathToSave = Application.persistentDataPath + "/Saves/" + saveName;
         
         _saveName = saveName;
         _saveText.text = _saveName;
         gameObject.name = _saveName;
+        
+        _lastModified = UniverseDirectories.LastTimeModified(_pathToSave + "/capture.png");
+        _dateText.text = _lastModified.ToString(CultureInfo.InvariantCulture);
+        
         _savePreviewContainer.transform.localScale.Set(.15f, .15f, .15f);
         _saveCapture = await UniversePictures.LoadSpriteFromPath(_pathToSave + "/capture.png", 1920, 1080);
         _savePreviewContainer.sprite = _saveCapture;
         
-        _lastModified = UniverseDirectories.LastTimeModified(_pathToSave + "/capture.png");
     }
 
     void Update()
