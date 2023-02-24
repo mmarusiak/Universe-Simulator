328080cb98c241107a6665ce9f78d161069cd466
Marcel Marusiak <marcel.marusiak@gmail.com>
2023-02-24 15:58:21 +0100

    WIP: Added simple working cutter prototype Added collider cutter. Need to still play with centering game object to new center.

--- Assets/Scripts/PlanetEditor/PlanetCutter/PlanetCut.cs
+++ Assets/Scripts/PlanetEditor/PlanetCutter/PlanetCut.cs
@@ -1,6 +1,7 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
 
 public class PlanetCut : MonoBehaviour
 {
@@ -28,65 +29,75 @@ public class PlanetCut : MonoBehaviour
         List<GameObject> planetsToCut = PlanetsOnLine(pointA, pointB);
         foreach (var planet in planetsToCut)
         {
-            Debug.Log(planet.transform.lossyScale);
+            // now we should create two new game objects -> calculate mass of them -> add a bit of force and offset to de-attach them
             var sprite = planet.GetComponent<SpriteMask>().sprite;
-            planet.GetComponent<SpriteMask>().sprite = SlicedSprite(sprite, pointA, pointB, planet.transform.position,
-                planet.transform.lossyScale.x / 2);
+            // intersection points
+            Vector2[] points = CalculateIntersectionPoints(sprite.texture, pointA, pointB, planet.transform.position,
+                planet.transform.lossyScale.x/2);
+            // slice sprite
+            planet.GetComponent<SpriteMask>().sprite = SlicedSprite(sprite, points[0], points[1])[0];
+            // slice collider
+            SliceCollider(planet, planet.GetComponent<SpriteMask>().sprite);
         }
     }
 
-    Sprite SlicedSprite(Sprite baseSprite, Vector2 worldPointA, Vector2 worldPointB, Vector2 planetPos, float radius)
+    void SliceCollider(GameObject planet, Sprite targetShape)
+    {
+        // Get the sprite's vertices and sort them in clockwise order
+        Vector2[] spriteVertices = targetShape.vertices;
+        spriteVertices = SortVerticesClockwise(spriteVertices);
+
+        Destroy(planet.GetComponent<Collider2D>());
+        // Set up the collider using the sorted vertices
+        PolygonCollider2D polygonCollider = planet.AddComponent<PolygonCollider2D>();
+        polygonCollider.points = spriteVertices;
+    }
+    
+    Sprite[] SlicedSprite(Sprite baseSprite, Vector2 a, Vector2 b)
     {
         // Get the texture of the sprite
         Texture2D texture = baseSprite.texture;
 
-        // Calculate the position of the slice line in texture coordinates
-        Vector2 startTextureCoord = baseSprite.textureRect.position;
-        Vector2 endTextureCoord = startTextureCoord + baseSprite.textureRect.size;
-
-        // Get the start and end points of the slice line in world space
-        Vector3 startWorldPoint = transform.position + transform.up * baseSprite.textureRect.yMin;
-        Vector3 endWorldPoint = transform.position + transform.up * baseSprite.textureRect.yMax;
-
-        // Convert the world space start and end points to texture coordinates
-        startTextureCoord.x += startWorldPoint.x / texture.width;
-        startTextureCoord.y += startWorldPoint.y / texture.height;
-        endTextureCoord.x += endWorldPoint.x / texture.width;
-        endTextureCoord.y += endWorldPoint.y / texture.height;
-
-        // Calculate the intersection points of the slice line with the edges of the texture
-        Vector2[] intersectionPoints =
-            CalculateIntersectionPoints(texture, worldPointA, worldPointB, planetPos, radius);
-
-        // Slice the texture along the line using the intersection points as the fixed points
-        Texture2D slicedTexture = SliceTexture(texture, intersectionPoints[0], intersectionPoints[1]);
-
-        // Create a new sprite using the sliced texture
-        Sprite slicedSprite = Sprite.Create(slicedTexture, new Rect(0, 0, slicedTexture.width, slicedTexture.height),
-            new Vector2(0.5f, 0.5f), baseSprite.pixelsPerUnit);
+        // Slice the textures along the line using the intersection points as the fixed points
+        Texture2D[] slicedTextures = SliceTexture(texture, a, b);
+        
+        // Create a new sprites using the sliced textures
+        Sprite[] slicedSprites = new Sprite[slicedTextures.Length];
+        for (int i = 0; i < slicedTextures.Length; i ++ )
+        {
+            Sprite slicedSprite = Sprite.Create(slicedTextures[i],
+                new Rect(0, 0, slicedTextures[i].width, slicedTextures[i].height),
+                new Vector2(0.5f, 0.5f), baseSprite.pixelsPerUnit);
+            slicedSprites[i] = slicedSprite;
+        }
 
         // Set the sliced sprite on the game object
-        return slicedSprite;
+        return slicedSprites;
     }
 
     // Helper method to slice a texture along a line
-    Texture2D SliceTexture(Texture2D texture, Vector2 startCoord, Vector2 endCoord)
+    Texture2D[] SliceTexture(Texture2D texture, Vector2 startCoord, Vector2 endCoord)
     {
         int width = texture.width;
         int height = texture.height;
 
         // Create a new texture to hold the sliced result
-        Texture2D slicedTexture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+        Texture2D[] slicedTexture = new[]
+        {
+            new Texture2D(width, height, TextureFormat.RGBA32, false),
+            new Texture2D(width, height, TextureFormat.RGBA32, false),
+        };
 
         // Copy the pixels from the original texture to the sliced texture
-        slicedTexture.SetPixels32(texture.GetPixels32());
+        slicedTexture[0].SetPixels32(texture.GetPixels32());
+        slicedTexture[1].SetPixels32(texture.GetPixels32());
 
         // Calculate the slope and y-intercept of the slice line
         float slope = (endCoord.y - startCoord.y) / (endCoord.x - startCoord.x);
         float yIntercept = startCoord.y - slope
             * startCoord.x;
 
-// Iterate over all pixels in the texture
+        // Iterate over all pixels in the texture
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
@@ -94,23 +105,25 @@ public class PlanetCut : MonoBehaviour
                 // Calculate the y-coordinate of the slice line at this x-coordinate
                 float sliceY = slope * x + yIntercept;
 
+                // Set the pixel to transparent
+                Color32 pixel = slicedTexture[0].GetPixel(x, y);
+                pixel.a = 0;
+                
                 // Check if the current pixel is above or below the slice line
-                if (y > sliceY)
-                {
-                    // Set the pixel to transparent
-                    Color32 pixel = slicedTexture.GetPixel(x, y);
-                    pixel.a = 0;
-                    slicedTexture.SetPixel(x, y, pixel);
-                }
+                if (y > sliceY) slicedTexture[1].SetPixel(x, y, pixel);
+                
+                else slicedTexture[0].SetPixel(x, y, pixel);
             }
         }
 
-// Apply the changes to the sliced texture
-        slicedTexture.Apply();
+        // Apply the changes to the sliced texture
+        slicedTexture[0].Apply();
+        slicedTexture[1].Apply();
 
         return slicedTexture;
     }
 
+    // Helper method to get intersection points
     Vector2[] CalculateIntersectionPoints(Texture2D texture, Vector2 worldPointA, Vector2 worldPointB,
         Vector2 planetPos, float radius)
     {
@@ -134,8 +147,6 @@ public class PlanetCut : MonoBehaviour
 
             float x2 = (-b + Mathf.Sqrt(delta)) / (2 * a);
             float y2 = slope * x2 + bias;
-            
-            Debug.Log($"{x1}, {y1} and {x2}, {y2}");
 
             // texture coordinates
             float diffx = planetPos.x - radius, diffy = planetPos.y - radius;
@@ -145,7 +156,6 @@ public class PlanetCut : MonoBehaviour
             float ty1 = (y1 - diffy) * scaler;
             float tx2 = (x2 - diffx) * scaler;
             float ty2 = (y2 - diffy) * scaler;
-            Debug.Log($"{tx1}, {ty1} and {tx2}, {ty2}");
             
             return new[]
             {
@@ -156,5 +166,25 @@ public class PlanetCut : MonoBehaviour
         
         return new []{Vector2.zero, Vector2.zero};
     }
+    
+    private Vector2[] SortVerticesClockwise(Vector2[] vertices)
+    {
+        // Calculate the centroid of the polygon
+        Vector2 centroid = Vector2.zero;
+        float signedArea = 0;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector2 vertex1 = vertices[i];
+            Vector2 vertex2 = vertices[(i + 1) % vertices.Length];
+            float crossProduct = (vertex1.x * vertex2.y) - (vertex1.y * vertex2.x);
+            signedArea += crossProduct;
+            centroid += (vertex1 + vertex2) * crossProduct;
+        }
+        signedArea *= 0.5f;
+        centroid /= (6 * signedArea);
 
+        // Sort the vertices in clockwise order around the centroid
+        Vector2[] sortedVertices = vertices.OrderBy(v => Mathf.Atan2(v.y - centroid.y, v.x - centroid.x)).ToArray();
+        return sortedVertices;
+    }
 }
